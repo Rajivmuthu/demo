7407d53d7479309a9f002d5d1557ee4b50c762bc
Rajiv M <rajivmuthu13@gmail.com>
2023-04-21 09:51:31 +0530

    add a file

---
+++ if-statement.js
@@ -0,0 +1,9 @@
+let fruit="Orange"
+
+
+if(fruit.length>5)
+{
+    console.log('The fruit name has more than five characters.')
+}else{
+    console.log('The fruits name has five characters or less.')
+}
