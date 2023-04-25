7348079a55c1aaac43d6c3f65f803b6f4e17291f
Rajiv M <rajivmuthu13@gmail.com>
2023-04-25 18:18:42 +0530

    add year.js

--- map,reduce,filter/year.js
+++ map,reduce,filter/year.js
@@ -1,9 +1,9 @@
-names=[{name:'Rajiv',dob:new Date('12/01/2001')},
-        {name:'aji',dob:new Date('02/07/2000')}]
+names=[{name:'Rajiv',dob:'12/02/2001'},
+        {name:'aji',dob:'02/07/2000'}]
 obj={}
 names.map((e)=>
     {
- let current=e.dob.getFullYear()
+ let current=new Date (e.dob).getFullYear()
 
     if(!(current in obj)){
         obj[current]=[]
